1e15447c50df67ee554f797dc1d7cd49b82121f4
sanal <sanalpanicker1987@gmail.com>
2014-05-11 14:46:38 -0700

    canvas API new tutorials

--- WebContent/scripts/app.js
+++ WebContent/scripts/app.js
@@ -5,5 +5,10 @@ app = {
 				$("#2").click(function(){canvas.initiate2();});
 				$("#3").click(function(){canvas.initiate3();});
 				$("#4").click(function(){canvas.initiate4();});
+				$("#5").click(function(){canvas.initiate5();});
+				$("#6").click(function(){canvas.initiate6();});
+				$("#7").click(function(){canvas.initiate7();});
+				$("#8").click(function(){canvas.initiate8();});
+				$("#9").click(function(){canvas.initiate9();});
 				}
 };
--- WebContent/scripts/canvas.js
+++ WebContent/scripts/canvas.js
@@ -36,6 +36,76 @@ canvas = {
 						//canvas.strokeRect(0,0,120,120);
 						//canvas.fillRect(10,10,100,100);
 						canvas.fillRect(150,10,200,100);
-					},			
-	
+					},
+		initiate5: function(){
+						var canvas = $("#canvas5")[0].getContext('2d');	//added[0] as this was throwing error 
+						canvas.beginPath();
+						canvas.moveTo(100,100);
+						canvas.lineTo(200,200);
+						canvas.lineTo(100,200);
+						canvas.stroke();			
+					},
+		initiate6: function(){
+						var canvas = $("#canvas6")[0].getContext('2d');	//added[0] as this was throwing error 
+						canvas.beginPath();
+						canvas.moveTo(100,100);
+						canvas.lineTo(200,200);
+						canvas.lineTo(100,200);
+						canvas.closePath();
+						canvas.fill();
+						canvas.stroke();			
+					},
+		initiate7: function(){
+						var canvas = $("#canvas7")[0].getContext('2d');	//added[0] as this was throwing error 
+						canvas.beginPath();
+						canvas.moveTo(100,100);
+						canvas.lineTo(200,200);
+						canvas.lineTo(100,200);
+						canvas.clip();
+						//canvas.closePath();
+						//canvas.fill();
+						canvas.beginPath();
+						for(var f=0;f<300;f=f+10){
+							canvas.moveTo(0,f);
+							canvas.lineTo(200,f);
+						}
+						canvas.stroke();			
+					},
+		initiate8: function(){
+						var canvas = $("#canvas8")[0].getContext('2d');	//added[0] as this was throwing error 
+						canvas.beginPath();
+						canvas.arc(100,100,50,0,Math.PI*2,false);
+						//canvas.beginPath();
+						canvas.moveTo(200,150);
+						canvas.arc(150,150,50,0,Math.PI*2,false);
+						
+						
+						//canvas.clip();
+						
+						//canvas.beginPath();
+						//for(var f=0;f<300;f=f+10){
+							//canvas.moveTo(0,f);
+							//canvas.lineTo(200,f);
+						//}
+						canvas.stroke();			
+					},
+		initiate9: function(){
+						var canvas = $("#canvas9")[0].getContext('2d');	//added[0] as this was throwing error 
+						canvas.beginPath();
+						var radians =  Math.PI/180*45;
+						canvas.arc(100,100,50,0,radians,true);//clockwise if fasle.
+						//canvas.beginPath();
+						////.moveTo(200,150);
+						//canvas.arc(150,150,50,0,Math.PI*2,false);
+						
+						
+						//canvas.clip();
+						
+						//canvas.beginPath();
+						//for(var f=0;f<300;f=f+10){
+							//canvas.moveTo(0,f);
+							//canvas.lineTo(200,f);
+						//}
+						canvas.stroke();			
+					},
 };
